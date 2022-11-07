452ee318bd2e75989fcf68616a6e6c78cd703536
tiguerchaessaid <72193212+tiguerchaessaid@users.noreply.github.com>
2022-11-07 16:07:17 +0100

    Add files via upload

---
+++ addTwoNumbers.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 14:30:03 2022
+
+@author: etiguercha
+"""
+
+def addTwoNumbers(l1,l2):
+    l=0
+    for i in range(l1):
+        l=l1[i]+l2[i]
+        if l>10:
+            
+            
+            
+            
